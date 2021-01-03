ae58d262af2d1eb6b4ef6d2941e4f9b487e61905
Dionysha <74178837+Dionysha@users.noreply.github.com>
2021-01-03 17:12:14 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+var shopItem ;
+
+shopItem = document.querySelector("section.shopitems img");
+
+shopItem.addEventListener("click", changeImage);
+
+function changeImage() {
+shopItem.classList.add("shopItem");
+}
